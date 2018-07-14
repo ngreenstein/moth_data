07821dfb91fe9cf889e30bc04445a956096c6b19
Nathan Greenstein <nathang345@gmail.com>
2018-07-13 17:40:40 -0700

    Add overwrite protection to pilot.serialize

--- pilot/__init__.py
+++ pilot/__init__.py
@@ -1,3 +1,3 @@
 pilot_config = {
-	"waves": ("1.0", "1.1")
+	"waves": ("1.0", "1.1") # If only one wave, make sure to keep a trailing comma
 }
--- pilot/serialize.py
+++ pilot/serialize.py
@@ -1,22 +1,30 @@
 import md_utils
-import argparse, csv
+import argparse, os, csv
 from . import pilot_config
 
-def serializeRatings(outfile):
-	# Run the query
-	waves = ",".join(map(lambda item: "'%s'" % item, pilot_config["waves"])) # Convert the list of waves into something like "'1.0', '1.1'"
-	query = open("pilot/ratings.sql", "r").read() % waves # Avoid escaping issues by preparing query by hand
-	cursor = md_utils.db().cursor()
-	cursor.execute(query)
+def serializeRatings(outfile, overwrite = False):
 	
-	# Save the CSV
-	writer = csv.writer(outfile)
-	writer.writerow([col[0] for col in cursor.description]) # Header row
-	writer.writerows(cursor.fetchall())
+	if not overwrite and os.path.isfile(outfile):
+		print "Unable to serialize ratings because output file '%s' already exists. Please delete the file or specify a different desination." % outfile
+		return False
 	
-	# Clean up
-	cursor.close()
-	outfile.close()
+	with open(outfile, "wb") as csvFile:
+	
+		# Run the query
+		waves = ",".join(map(lambda item: "'%s'" % item, pilot_config["waves"])) # Convert the list of waves into something like "'1.0', '1.1'"
+		query = open("pilot/ratings.sql", "r").read() % waves # Avoid escaping issues by preparing query by hand
+		cursor = md_utils.db().cursor()
+		cursor.execute(query)
+		
+		# Save the CSV
+		writer = csv.writer(csvFile)
+		writer.writerow([col[0] for col in cursor.description]) # Header row
+		writer.writerows(cursor.fetchall())
+		
+		# Clean up
+		cursor.close()
+	
+	return True
 	
 	
 def serializeStimuli(outfile):
@@ -27,7 +35,7 @@ if __name__ == "__main__":
 	# Create parser for command-line interface
 	parser = argparse.ArgumentParser(description = "Serializes moth pilot data. Generates a CSV file representing either ratings or stimuli.")
 	# Both actions require an output file
-	parser.add_argument("outfile", type = argparse.FileType("w"), help = "The destination file. Should be writeable and of type `.csv`.")
+	parser.add_argument("outfile", help = "The destination file. Should be writeable and of type `.csv`.")
 	# The action taken depends on arguments: `--ratings` flag for ratings and `--stimuli` flag for stimuli
 	# Action is determined by the value of `operation`, a pointer to the relevant function
 	# Require exacty one of `--ratings` and `--stimuli`
@@ -35,8 +43,9 @@ if __name__ == "__main__":
 	# Change the `operation` pointer based on the presence of a `--ratings` or `--stimuli` flag
 	actionGroup.add_argument("--ratings", action = "store_const", const = serializeRatings, dest = "operation", help = "serialize ratings data")
 	actionGroup.add_argument("--stimuli", action = "store_const", const = serializeStimuli, dest = "operation", help = "serialize stimuli data")
+	parser.add_argument("--overwrite", action = "store_true", help = "Whether to overwrite an existing file. Defaults to no.")
 	
 	# Kick off the parser
 	args = parser.parse_args()
 	# Call the relevant operation, and pass in the specified outfile
-	args.operation(args.outfile)
+	args.operation(outfile = args.outfile, overwrite = args.overwrite)
